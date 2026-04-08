36f9d433899f815e6a25bc5c94087215e580d33b
ammar11313118 <ammarziya16@gmail.com>
2026-04-08 14:33:59 +0530

    fix vite build error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
+import { Analytics } from "@vercel/analytics/next"
 
 
 import './assets/styles.css'; // Add Tailwind styles if configured
